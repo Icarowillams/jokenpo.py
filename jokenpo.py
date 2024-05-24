6a6c26a7c1dc55b20bdb403c67c39e85977fd91b
ICARO RAMOS <119986789+Icarowillams@users.noreply.github.com>
2024-05-24 14:14:24 -0300

    Jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -1,29 +1,39 @@
-def jogarJokenpo():
-  jogador1 = input("Jogador 1, escolha entre pedra, papel ou tesoura: ")
-  jogador2 = input("Jogador 2, escolha entre pedra, papel ou tesoura: ")
+class Jokenpo:
+    def __init__(self):
+        self.jogador1 = ""
+        self.jogador2 = ""
 
-  if jogador1 == jogador2:
-      print("Empate!")
-  elif jogador1 == "pedra":
-      if jogador2 == "tesoura":
-          print("Jogador 1 ganha!")
-      else:
-          print("Jogador 2 ganha!")
-  elif jogador1 == "papel":
-      if jogador2 == "pedra":
-          print("Jogador 1 ganha!")
-      else:
-          print("Jogador 2 ganha!")
-  elif jogador1 == "tesoura":
-      if jogador2 == "papel":
-          print("Jogador 1 ganha!")
-      else:
-          print("Jogador 2 ganha!")
-  else:
-      print("Escolha inválida. Certifique-se de digitar 'pedra', 'papel' ou 'tesoura'.")
+    def obterEscolhas(self):
+        self.jogador1 = input("Jogador 1, escolha entre pedra, papel ou tesoura: ")
+        self.jogador2 = input("Jogador 2, escolha entre pedra, papel ou tesoura: ")
 
-while True:
-  jogarJokenpo()
-  jogar_novamente = input("Desejam jogar novamente? (s/n): ")
-  if jogar_novamente != "s":
-      break
+    def determinarVencedor(self):
+        if self.jogador1 == self.jogador2:
+            print("Empate!")
+        elif self.jogador1 == "pedra":
+            if self.jogador2 == "tesoura":
+                print("Jogador 1 ganha!")
+            else:
+                print("Jogador 2 ganha!")
+        elif self.jogador1 == "papel":
+            if self.jogador2 == "pedra":
+                print("Jogador 1 ganha!")
+            else:
+                print("Jogador 2 ganha!")
+        elif self.jogador1 == "tesoura":
+            if self.jogador2 == "papel":
+                print("Jogador 1 ganha!")
+            else:
+                print("Jogador 2 ganha!")
+        else:
+            print("Escolha inválida. Certifique de digitar 'pedra', 'papel' ou 'tesoura'.")
+
+    def jogar(self):
+        while True:
+            self.obterEscolhas()
+            self.determinarVencedor()
+            jogar_novamente = input("Desejam jogar novamente? (s/n): ")
+            if jogar_novamente != "s":
+                break
+jogo = Jokenpo()
+jogo.jogar()
